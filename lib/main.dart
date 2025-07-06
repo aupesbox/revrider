c4418d2b12dc543d7cb863526e4b8efdab461e1e
DELL <vikram51@yahoo.com>
2025-07-06 12:25:32 +0530

    5 july 25

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
 
 import 'providers/theme_provider.dart';
 import 'providers/activation_provider.dart';
@@ -11,6 +12,9 @@ import 'utils/themes.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Purchases.configure(
+    PurchasesConfiguration("public_sdk_key_here"),
+  );
   // Load theme choice
   final themeProvider = ThemeProvider();
   await themeProvider.load();
